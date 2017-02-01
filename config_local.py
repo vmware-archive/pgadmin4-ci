4eb13659cfd572ecd3a7e7dee5ae7a6b2ef7837e
George Gelashvili and Tira Odhner <pair+ggelashvili+aodhner@pivotal.io>
2017-02-01 14:44:45 -0500

    Add a test config and config_local, and copy them into the submodule

---
+++ config_local.py
@@ -0,0 +1,26 @@
+from config import *
+
+DEBUG = True
+
+# Enable the test module
+MODULE_BLACKLIST.remove('test')
+# Log
+CONSOLE_LOG_LEVEL = DEBUG
+FILE_LOG_LEVEL = DEBUG
+
+DEFAULT_SERVER = '127.0.0.1'
+
+UPGRADE_CHECK_ENABLED = True
+
+SERVER_MODE = False
+# Use a different config DB for each server mode.
+if SERVER_MODE == False:
+    SQLITE_PATH = os.path.join(
+        DATA_DIR,
+        'pgadmin4-desktop.db'
+    )
+else:
+    SQLITE_PATH = os.path.join(
+        DATA_DIR,
+        'pgadmin4-server.db'
+    )
