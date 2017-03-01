80d2ff7943ba5ac9807a3afd43e781d4f5bf03aa
Joao Pereira and Tira Odhner <pair+jpereira+aodhner@pivotal.io>
2017-03-01 12:05:16 -0500

    Moved all steps into run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
+case $CIRCLE_NODE_INDEX in
+    0)
+        pyenv global 2.7.12
+    ;;
+    1)
+        pyenv global 3.1.5
+    ;;
+    2)
+        pyenv global 3.2.6
+    ;;
+    3)
+        pyenv global 3.5.2
+    ;;
+esac
+
+pip install -r ./submodules/plummaster/requirements.txt
+
 python ./submodules/master/web/regression/runtests.py
 python ./submodules/plummaster/web/regression/runtests.py
