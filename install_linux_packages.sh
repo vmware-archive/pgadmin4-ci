02207d399c3760880961fae6d32a6192db3c7b6c
Tira Odhner <pair+aodhner@pivotal.io>
2017-03-15 14:50:43 -0400

    Actually working Chrome updating

--- install_linux_packages.sh
+++ install_linux_packages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# copy utility for pyperclip to work in the test
 sudo apt-get install xsel
 
-sudo apt-get upgrade google-chrome-stable
+# update google chrome so it works with new chromedriver
+curl -s https://raw.githubusercontent.com/chronogolf/circleci-google-chrome/master/use_chrome_stable_version.sh | bash
