918f7349ca309ff4b18e1730ab303f866d46ac26
Joao Pereira and Sarah McAlear <pair+jpereira+smcalear@pivotal.io>
2017-03-23 17:39:51 -0400

    Change test configuration structure to use multiple variables instead of one

--- run_plummaster_on_greenplum_tests.sh
+++ run_plummaster_on_greenplum_tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-sed -e "s/SERVER_CONFIGURATION/${GREENPLUM_CONFIGURATION}/g" test_config.template.json > ./submodules/plummaster/web/regression/test_config.json
-
-ls -ltr ./submodules/plummaster/web/regression/
-
-cat ./submodules/plummaster/web/regression/test_config.json
+sed -e "s/GREENPLUM_DB_HOST/${GREENPLUM_DB_HOST}/g" \
+    -e "s/GREENPLUM_DB_USERNAME/${GREENPLUM_DB_USERNAME}/g" \
+    -e "s/GREENPLUM_DB_PASSWORD/${GREENPLUM_DB_PASSWORD}/g" \
+    -e "s/GREENPLUM_DB_PORT/${GREENPLUM_DB_PORT}/g" \
+    test_config.template.json > ./submodules/plummaster/web/regression/test_config.json
 
 python ./submodules/plummaster/web/regression/runtests.py --pkg feature_tests
