0f041135ac9786e0dbd137cb88c8b17bf5222e6f
Joao Pereira and Sarah McAlear <pair+jpereira+smcalear@pivotal.io>
2017-04-11 14:18:23 -0400

    Add branch_name parameter to execute CI on all branches

--- delayed_autocommit_submodule_bump.sh
+++ delayed_autocommit_submodule_bump.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This should be invoked by the pgadmin4/.git/hooks/pre-push hook
-
+branch_name=$1
 set -euxo pipefail
 echo 'Resting before commit to pgadmin4-CI'
 
@@ -12,7 +12,7 @@ git submodule update --init
 
 pushd submodules/plummaster
   git fetch
-  git reset --hard origin/plummaster
+  git reset --hard origin/$branch_name
   CHILD_SHA=$(git rev-parse HEAD)
   CHILD_MESSAGE=$(git log --format=%B -n 1 HEAD)
 popd
