3276b8c149698aa9d81f11640455235f8e3e4095
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 14:17:17 -0400

    Add karma run

--- tasks/run_pip.sh
+++ tasks/run_pip.sh
@@ -3,3 +3,8 @@ pip -v
 pip install -r pivotal-source/requirements.txt
 
 ./pipeline-ci/tasks/install_linux_packages.sh
+
+pushd ./pivotal-source/web/
+    yarn
+    yarn run karma start -- --single-run
+popd
