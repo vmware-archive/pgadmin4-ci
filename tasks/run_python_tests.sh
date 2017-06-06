cdf396359a57b84a1b350e4ee16e3488f5c7c621
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 17:15:22 -0400

    Add postgres user config

--- tasks/run_python_tests.sh
+++ tasks/run_python_tests.sh
@@ -6,6 +6,10 @@ pip install -r pivotal-source/web/regression/requirements.txt
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
 apt-get update
-apt-get install postgresql postgresql-contrib
+apt-get -y install postgresql postgresql-contrib
+
+sed -i s/md5/trust/ /etc/postgresql/9.6/main/pg_hba.conf
+
+/etc/init.d/postgresql restart
 
 python ./pivotal-source/web/regression/runtests.py
