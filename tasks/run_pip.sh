29fc709d1d7f197cfaa177c50fd448ef47be99d8
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 15:29:12 -0400

    Use a different docker image

--- tasks/run_pip.sh
+++ tasks/run_pip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pip -v
 pip install -r pivotal-source/requirements.txt
+pip install -r pivotal-source/web/regression/requirements.txt
 
-./pipeline-ci/tasks/install_linux_packages.sh
+#./pipeline-ci/tasks/install_linux_packages.sh
 
 cd ./pivotal-source/web/
 yarn
