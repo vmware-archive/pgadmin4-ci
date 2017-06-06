bcffed153e8b1e0f8c1fea8c8a46837688332b64
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 14:06:13 -0400

    Comment chrome installation script

--- tasks/install_linux_packages.sh
+++ tasks/install_linux_packages.sh
@@ -7,7 +7,7 @@ apt-get install xsel
 # update google chrome so it works with new chromedriver
 #curl -s https://raw.githubusercontent.com/chronogolf/circleci-google-chrome/master/use_chrome_stable_version.sh | bash
 
-curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-dpkg -i google-chrome.deb;
-sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
-rm google-chrome.deb
+#curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+#dpkg -i google-chrome.deb;
+#sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
+#rm google-chrome.deb
