e429a83508f2b2a2cedf09da72ad559a6943b886
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 16:45:37 -0400

    Add postgres installation

--- tasks/run_python_tests.sh
+++ tasks/run_python_tests.sh
@@ -3,4 +3,9 @@
 pip install -r pivotal-source/requirements.txt
 pip install -r pivotal-source/web/regression/requirements.txt
 
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
+apt-get update
+apt-get install postgresql postgresql-contrib
+
 python ./pivotal-source/web/regression/runtests.py
