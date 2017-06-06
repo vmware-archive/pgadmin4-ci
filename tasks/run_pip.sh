12e4eb608caf8b001f301979a98873c2848c588f
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 15:00:46 -0400

    Update script that runs tests to have karma as last command

--- tasks/run_pip.sh
+++ tasks/run_pip.sh
@@ -4,7 +4,6 @@ pip install -r pivotal-source/requirements.txt
 
 ./pipeline-ci/tasks/install_linux_packages.sh
 
-pushd ./pivotal-source/web/
-    yarn
-    yarn run karma start -- --single-run
-popd
+cd ./pivotal-source/web/
+yarn
+yarn run karma start -- --single-run
