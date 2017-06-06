7b8bf864864ee95df74f36337a9411726d3cdb56
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 14:13:41 -0400

    Add installation of chrome using scripts

--- tasks/install_linux_packages.sh
+++ tasks/install_linux_packages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # copy utility for pyperclip to work in the test
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
 apt-get install xsel
 
@@ -11,3 +13,16 @@ apt-get install xsel
 #dpkg -i google-chrome.deb;
 #sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
 #rm google-chrome.deb
+
+apt-get -y install wget --fix-missing
+apt-get -y install xvfb --fix-missing # chrome will use this to run headlessly
+apt-get -y install unzip --fix-missing
+
+# install dbus - chromedriver needs this to talk to google-chrome
+apt-get -y install dbus --fix-missing
+apt-get -y install dbus-x11 --fix-missing
+ln -s /bin/dbus-daemon /usr/bin/dbus-daemon     # /etc/init.d/dbus has the wrong location
+ln -s /bin/dbus-uuidgen /usr/bin/dbus-uuidgen   # /etc/init.d/dbus has the wrong location
+
+# install chrome
+apt-get -y install google-chrome-stable
