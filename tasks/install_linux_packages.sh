c22629756fb48d76ff6bed077ac04ddcf7ab3e5c
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-06 13:51:20 -0400

    Remove curl and move the code directly into our script

--- tasks/install_linux_packages.sh
+++ tasks/install_linux_packages.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # copy utility for pyperclip to work in the test
-sudo apt-get install xsel
+apt-get install xsel
 
 # update google chrome so it works with new chromedriver
-curl -s https://raw.githubusercontent.com/chronogolf/circleci-google-chrome/master/use_chrome_stable_version.sh | bash
+#curl -s https://raw.githubusercontent.com/chronogolf/circleci-google-chrome/master/use_chrome_stable_version.sh | bash
+
+curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+dpkg -i google-chrome.deb;
+sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
+rm google-chrome.deb
