e59ae11f28ff2039c71e9857d88885b7d8862ac3
Matt Kleiman and Shruti Iyer <pair+mkleiman+siyer@pivotal.io>
2017-06-07 10:28:11 -0400

    Remove disused install statements

--- tasks/run_karma.sh
+++ tasks/run_karma.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#pip install -r pivotal-source/requirements.txt
-#pip install -r pivotal-source/web/regression/requirements.txt
-
-#./pipeline-ci/tasks/install_linux_packages.sh
 
 cd ./pivotal-source/web/
 yarn
