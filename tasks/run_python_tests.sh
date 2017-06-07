9fc92f315786c57e7b05b19319df4fea1c75275d
Matt Kleiman and Shruti Iyer <pair+mkleiman+siyer@pivotal.io>
2017-06-07 16:39:47 -0400

    Install Chromedriver

--- tasks/run_python_tests.sh
+++ tasks/run_python_tests.sh
@@ -10,11 +10,24 @@ pip install -r pivotal-source/requirements.txt
 pip install -r pivotal-source/web/regression/requirements.txt
 
 # Install chrome
-apt-get -y remove google-chrome-stable # TODO: Remove it from the actual Dockerfile
-apt-get -y install xvfb chromium
+apt-get -y remove google-chrome-stable # TODO: Remove this line from the actual Dockerfile
+apt-get -y install xvfb
+apt-get -y install chromium
 cp pipeline-ci/tasks/xvfb-chromium /usr/bin/xvfb-chromium
 ln -s /usr/bin/xvfb-chromium /usr/bin/google-chrome
 ln -s /usr/bin/xvfb-chromium /usr/bin/chromium-browser
+
+#
+apt-get -y install curl
+apt-get -y install unzip
+#apt-get -y install libgconf-2-4
+CHROMEDRIVER_VERSION="2.28"
+CHROMEDRIVER_SHA256="8f5b0ab727c326a2f7887f08e4f577cb4452a9e5783d1938728946a8557a37bc"
+
+curl -SLO "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip" \
+  && echo "$CHROMEDRIVER_SHA256  chromedriver_linux64.zip" | sha256sum -c - \
+  && unzip "chromedriver_linux64.zip" -d /usr/local/bin \
+  && rm "chromedriver_linux64.zip"
 
 # Install postgres
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
