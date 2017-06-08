6460a3c3d240a50339043aba204052c182f4b502
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-08 14:16:43 -0400

    WIP: Change the docker image to use awesome selenium image

--- tasks/run_python_tests.sh
+++ tasks/run_python_tests.sh
@@ -10,12 +10,12 @@ pip install -r pivotal-source/requirements.txt
 pip install -r pivotal-source/web/regression/requirements.txt
 
 # Install chrome
-apt-get -y remove google-chrome-stable # TODO: Remove this line from the actual Dockerfile
-apt-get -y install xvfb
-apt-get -y install chromium
-cp pipeline-ci/tasks/xvfb-chromium /usr/bin/xvfb-chromium
-ln -s /usr/bin/xvfb-chromium /usr/bin/google-chrome
-ln -s /usr/bin/xvfb-chromium /usr/bin/chromium-browser
+#apt-get -y remove google-chrome-stable # TODO: Remove this line from the actual Dockerfile
+#apt-get -y install xvfb
+#apt-get -y install chromium
+#cp pipeline-ci/tasks/xvfb-chromium /usr/bin/xvfb-chromium
+#ln -s /usr/bin/xvfb-chromium /usr/bin/google-chrome
+#ln -s /usr/bin/xvfb-chromium /usr/bin/chromium-browser
 
 #
 apt-get -y install curl
@@ -41,6 +41,8 @@ sed -i s/md5/trust/ /etc/postgresql/9.4/main/pg_hba.conf
 # Pass in the config file
 cp pipeline-ci/config_local.py pivotal-source/web/config_local.py
 cp pipeline-ci/test_config.json pivotal-source/web/regression/test_config.json
+
+/opt/bin/start_selenium.sh &
 
 # Run all python tests. Includes feature tests.
 #python ./pivotal-source/web/regression/runtests.py
