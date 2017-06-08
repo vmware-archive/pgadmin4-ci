cc36eabd3ea86fe1cf9486a9731d228370316d87
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-08 10:22:34 -0400

    WIP remove packages already installed by image markadams/...

--- tasks/install_linux_packages.sh
+++ tasks/install_linux_packages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # chrome apt repository
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+#sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 # yarn apt repository
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
@@ -20,18 +20,18 @@ apt-get -y install xsel
 #sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
 #rm google-chrome.deb
 
-apt-get -y install wget --fix-missing
-apt-get -y install xvfb --fix-missing # chrome will use this to run headlessly
-apt-get -y install unzip --fix-missing
-
-# install dbus - chromedriver needs this to talk to google-chrome
-apt-get -y install dbus --fix-missing
-apt-get -y install dbus-x11 --fix-missing
-ln -s /bin/dbus-daemon /usr/bin/dbus-daemon     # /etc/init.d/dbus has the wrong location
-ln -s /bin/dbus-uuidgen /usr/bin/dbus-uuidgen   # /etc/init.d/dbus has the wrong location
+#apt-get -y install wget --fix-missing
+#apt-get -y install xvfb --fix-missing # chrome will use this to run headlessly
+#apt-get -y install unzip --fix-missing
+#
+## install dbus - chromedriver needs this to talk to google-chrome
+#apt-get -y install dbus --fix-missing
+#apt-get -y install dbus-x11 --fix-missing
+#ln -s /bin/dbus-daemon /usr/bin/dbus-daemon     # /etc/init.d/dbus has the wrong location
+#ln -s /bin/dbus-uuidgen /usr/bin/dbus-uuidgen   # /etc/init.d/dbus has the wrong location
 
 # install chrome
-apt-get -y install google-chrome-stable
+#apt-get -y install google-chrome-stable
 
 # install node
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
