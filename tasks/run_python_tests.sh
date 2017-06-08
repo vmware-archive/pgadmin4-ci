4a5104390e3fcfa4cde5c0c3c438809de15089c5
Joao Pereira and Shruti Iyer <pair+jpereira+siyer@pivotal.io>
2017-06-08 16:44:12 -0400

    WIP: Replace chrome driver with RemoteDriver to connect to Selenium

--- tasks/run_python_tests.sh
+++ tasks/run_python_tests.sh
@@ -9,25 +9,8 @@ apt-get -y install vim
 pip install -r pivotal-source/requirements.txt
 pip install -r pivotal-source/web/regression/requirements.txt
 
-# Install chrome
-#apt-get -y remove google-chrome-stable # TODO: Remove this line from the actual Dockerfile
-#apt-get -y install xvfb
-#apt-get -y install chromium
-#cp pipeline-ci/tasks/xvfb-chromium /usr/bin/xvfb-chromium
-#ln -s /usr/bin/xvfb-chromium /usr/bin/google-chrome
-#ln -s /usr/bin/xvfb-chromium /usr/bin/chromium-browser
-
-#
 apt-get -y install curl
 apt-get -y install unzip
-#apt-get -y install libgconf-2-4
-#CHROMEDRIVER_VERSION="2.28"
-#CHROMEDRIVER_SHA256="8f5b0ab727c326a2f7887f08e4f577cb4452a9e5783d1938728946a8557a37bc"
-#
-#curl -SLO "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip" \
-#  && echo "$CHROMEDRIVER_SHA256  chromedriver_linux64.zip" | sha256sum -c - \
-#  && unzip -o "chromedriver_linux64.zip" -d /usr/local/bin \
-#  && rm "chromedriver_linux64.zip"
 
 # Install postgres
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
@@ -41,6 +24,11 @@ sed -i s/md5/trust/ /etc/postgresql/9.5/main/pg_hba.conf
 # Pass in the config file
 cp pipeline-ci/config_local.py pivotal-source/web/config_local.py
 cp pipeline-ci/test_config.json pivotal-source/web/regression/test_config.json
+
+# Replace the first line of the file with the missing import
+sed -i "1s/.*/from selenium.webdriver.common.desired_capabilities import DesiredCapabilities/" ./pivotal-source/web/regression/runtests.py
+sed -i "s/Chrome()/Remote\(command_executor='http:\/\/127.0.0.1:4444\/wd\/hub', desired_capabilities=DesiredCapabilities.CHROME\)/)/" ./pivotal-source/web/regression/runtests.py
+
 
 /opt/bin/start_selenium.sh &
 
