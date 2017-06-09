8dd4ec806ce52f60bc5e13be10c2156d9ac78478
Joao Pereira and Sarah McAlear <pair+jpereira+smcalear@pivotal.io>
2017-06-09 10:28:30 -0400

    Adds a pyenv to the Docker image

---
+++ another_docker/pyenv_installation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+source ~/.bash_profile
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+pyenv install 3.6.0
+pyenv virtualenv 3.6.0 pgadmin
+pyenv activate pgadmin
