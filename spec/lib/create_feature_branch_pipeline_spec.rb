0e26fc20314b18be6847ff60143678df186e9db4
George Gelashvili and Sarah McAlear <pair+ggelashvili+smcalear@pivotal.io>
2017-06-13 17:51:12 -0400

    Add job for the python tests to generated pipeline
    needed to generate yaml with multiple jobs when present

--- spec/lib/create_feature_branch_pipeline_spec.rb
+++ spec/lib/create_feature_branch_pipeline_spec.rb
@@ -3,38 +3,42 @@ require 'create_feature_branch_pipeline'
 
 describe 'create' do
 
+  before(:each) do
+    @template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
+    @resources = Dir.pwd() + '/pipelines/resources_template.yml.erb'
+    @config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+  end
+
   it 'should output valid yaml' do
-    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
-    resources = Dir.pwd() + '/pipelines/resources_template.yml'
-    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+    CreateFeatureBranchPipeline.createFrom(@template, @resources, @config_file, ['branch1', 'branch2'])
 
-    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ['branch1', 'branch2'])
-
-    config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
-    expect(config_file['jobs'][0]['name']).to eq 'branch1-javascript-tests'
-    expect(config_file['jobs'][1]['name']).to eq 'branch2-javascript-tests'
+    generated_pipeline = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
+    job_names = generated_pipeline['jobs'].map { |job| job['name'] }
+    expect(job_names).to include 'branch1-javascript-tests'
   end
 
   it 'should strip newlines' do
-    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
-    resources = Dir.pwd() + '/pipelines/resources_template.yml'
-    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+    CreateFeatureBranchPipeline.createFrom(@template, @resources, @config_file, ["branch1\n", "branch2\n"])
 
-    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ["branch1\n", "branch2\n"])
+    generated_pipeline = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
+    expect(generated_pipeline['jobs'][0]['name']).to eq 'branch1-javascript-tests'
+  end
 
-    config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
-    expect(config_file['jobs'][0]['name']).to eq 'branch1-javascript-tests'
+  it 'should include multiple jobs' do
+    CreateFeatureBranchPipeline.createFrom(@template, @resources, @config_file, ['branch1', 'branch2'])
+
+    generated_pipeline = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
+    job_names = generated_pipeline['jobs'].map { |job| job['name'] }
+    expect(job_names).to include 'branch1-javascript-tests'
+    expect(job_names).to include 'branch2-javascript-tests'
+    expect(job_names).to include 'branch1-python-tests'
+    expect(job_names).to include 'branch2-python-tests'
   end
 
   it 'should include the resource information' do
-    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
-    resources = Dir.pwd() + '/pipelines/resources_template.yml'
-    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+    CreateFeatureBranchPipeline.createFrom(@template, @resources, @config_file, ['branch1', 'branch2'])
 
-    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ['branch1', 'branch2'])
-
-    config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
-
-    expect(config_file['resources'][0]['name']).to eq 'pivotal-source'
+    generated_pipeline = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
+    expect(generated_pipeline['resources'][0]['name']).to eq 'pivotal-source'
   end
 end
