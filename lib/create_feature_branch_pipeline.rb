ac2c3a357c47e15907a971e80b649c44433aae33
George Gelashvili and Sarah McAlear <pair+ggelashvili+smcalear@pivotal.io>
2017-06-13 15:49:49 -0400

    change jobs template file to run JS tests

--- lib/create_feature_branch_pipeline.rb
+++ lib/create_feature_branch_pipeline.rb
@@ -1,13 +1,14 @@
 require 'erb'
 require 'yaml'
+require 'json'
 
 class CreateFeatureBranchPipeline
-  def self.createFrom(yaml_erb_input_path, yaml_destination_path, branches_list)
+  def self.createFrom(yaml_erb_input_path, resources_template_path, yaml_destination_path, branches_list)
     branches = branches_list.map { |branch| branch.strip()}
 
     template = File.read(yaml_erb_input_path)
 
-    constructed_yaml = construct_jobs_for_branches(branches, template)
+    constructed_yaml = construct_jobs_for_branches(branches, template, resources_template_path)
 
     File.open(yaml_destination_path, 'w') do |f|
       f.write "# GENERATED, DO NOT TOUCH.\n"
@@ -16,9 +17,12 @@ class CreateFeatureBranchPipeline
     end
   end
 
-  def self.construct_jobs_for_branches(branches_list, template)
+  def self.construct_jobs_for_branches(branches_list, template, resources_template_path)
     renderer = ERB.new(template)
     constructed_yaml = {'jobs' => []}
+
+    resources = YAML.load(File.read(resources_template_path))
+    constructed_yaml['resources'] = resources['resources']
 
     branches_list.each do |branch|
       render_branch_name = branch
@@ -26,6 +30,7 @@ class CreateFeatureBranchPipeline
       job_for_branch = templated_yaml['jobs'][0]
       constructed_yaml['jobs'].push job_for_branch
     end
+
     constructed_yaml
   end
 
--- spec/lib/create_feature_branch_pipeline_spec.rb
+++ spec/lib/create_feature_branch_pipeline_spec.rb
@@ -4,17 +4,37 @@ require 'create_feature_branch_pipeline'
 describe 'create' do
 
   it 'should output valid yaml' do
-    CreateFeatureBranchPipeline.create(['branch1', 'branch2'])
+    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
+    resources = Dir.pwd() + '/pipelines/resources_template.yml'
+    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+
+    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ['branch1', 'branch2'])
 
     config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
     expect(config_file['jobs'][0]['name']).to eq 'branch1-javascript-tests'
     expect(config_file['jobs'][1]['name']).to eq 'branch2-javascript-tests'
   end
 
-  it ('should strip newlines') do
-    CreateFeatureBranchPipeline.create(["branch1\n", "branch2\n"])
+  it 'should strip newlines' do
+    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
+    resources = Dir.pwd() + '/pipelines/resources_template.yml'
+    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+
+    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ["branch1\n", "branch2\n"])
 
     config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
     expect(config_file['jobs'][0]['name']).to eq 'branch1-javascript-tests'
+  end
+
+  it 'should include the resource information' do
+    template = Dir.pwd() + '/pipelines/feature-branch-pipeline.yml.erb'
+    resources = Dir.pwd() + '/pipelines/resources_template.yml'
+    config_file = Dir.pwd() + '/pipelines/generated-feature-branch-pipeline.yml'
+
+    CreateFeatureBranchPipeline.createFrom(template, resources, config_file, ['branch1', 'branch2'])
+
+    config_file = YAML.load_file('pipelines/generated-feature-branch-pipeline.yml')
+
+    expect(config_file['resources'][0]['name']).to eq 'pivotal-source'
   end
 end
