03c15119382bba048749d4c83620434a79efc7ff
George Gelashvili and Sarah McAlear <pair+ggelashvili+smcalear@pivotal.io>
2017-06-13 15:49:49 -0400

    change jobs template file to run JS tests

--- scripts/generate_pipeline.rb
+++ scripts/generate_pipeline.rb
@@ -6,7 +6,12 @@ require_relative this_dir + '../lib/create_feature_branch_pipeline'
 branch_list_filename = cwd + ARGV[0]
 target_file = cwd + ARGV[1]
 branches = File.readlines branch_list_filename
+jobs_template = this_dir + '../pipelines/feature-branch-pipeline.yml.erb'
+resources_template = this_dir + '../pipelines/resources_template.yml.erb'
 
-CreateFeatureBranchPipeline.createFrom(this_dir + '../pipelines/feature-branch-pipeline.yml.erb',
+CreateFeatureBranchPipeline.createFrom(jobs_template,
+                                       resources_template,
                                        target_file,
                                        branches)
+
+puts File.read(target_file)
