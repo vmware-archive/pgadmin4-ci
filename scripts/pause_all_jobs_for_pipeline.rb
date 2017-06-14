9d14e3f341c2ba5942c9d38d5f7a8a88fb0792d3
George Gelashvili <pair+ggelashvili@pivotal.io>
2017-06-14 13:21:05 -0400

    create script to pause all jobs for a pipeline

---
+++ scripts/pause_all_jobs_for_pipeline.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'yaml'
+require 'byebug'
+
+$concourse_target = ARGV[0]
+$pipeline_name = ARGV[1]
+
+def get_pipeline_config
+  `fly -t #{$concourse_target} get-pipeline -p #{$pipeline_name}`
+end
+
+def list_of_jobs_from_config(pipeline_yaml)
+  pipeline_yaml['jobs'].map { |job| job['name'] }
+end
+
+def pause_job(job)
+  `fly -t #{$concourse_target} pause-job -j #{$pipeline_name}/#{job}`
+end
+
+pipeline_yaml = YAML.load get_pipeline_config
+
+jobs = list_of_jobs_from_config pipeline_yaml
+jobs.each {|job| pause_job(job)}
