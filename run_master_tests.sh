d231e1ad832b146c5c38d96fe6cc8d28d406c498
George Gelashvili and Shruti Iyer <pair+ggelashvili+siyer@pivotal.io>
2017-06-15 15:21:46 -0400

    delete some old scripts and circle-specific resources

--- run_master_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd ./submodules/master/web/
-    yarn
-    yarn run karma start -- --single-run
-popd
-
-python ./submodules/master/web/regression/runtests.py
--- run_plummaster_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-pushd ./submodules/plummaster/web/
-    yarn
-    yarn run karma start -- --single-run
-popd
-
-python ./submodules/plummaster/web/regression/runtests.py
