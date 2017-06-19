08ae90dff2f6341ba5ef71073a7c32512cef207d
Sarah McAlear and Shruti Iyer <pair+smcalear+siyer@pivotal.io>
2017-06-19 11:16:46 -0400

    try what happens if we install a different psql version and runtests again

--- scripts/run_python_tests.sh
+++ scripts/run_python_tests.sh
@@ -54,3 +54,14 @@ function runTests {
 }
 
 runTests
+
+# Install postgres
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
+apt-get -y install postgresql postgresql-contrib
+
+# Configure our instance of postgres
+sed -i s/md5/trust/ /etc/postgresql/9.2/main/pg_hba.conf
+/etc/init.d/postgresql restart
+
+runTests
