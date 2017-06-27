4268d296bb8dd46e6093bfa1179a3c7e47b7bda8
Sarah McAlear and Shruti Iyer <pair+smcalear+siyer@pivotal.io>
2017-06-27 17:23:20 -0400

    WIP revert python test docker image

--- docker/chrome-python/start_selenium.sh
+++ docker/chrome-python/start_selenium.sh
@@ -20,7 +20,7 @@ SERVERNUM=$(get_server_num)
 rm -f /tmp/.X*lock
 
 xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar browserName=chrome,platform=LINUX \
   ${SE_OPTS} &
 NODE_PID=$!
 
