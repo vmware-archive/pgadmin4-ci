27d4fb8765e403a80b8bff70d0308b2c80809ab3
George Gelashvili and Shruti Iyer <pair+ggelashvili+siyer@pivotal.io>
2017-06-28 17:14:54 -0400

    WIP make python tests run again

--- scripts/run_python_tests.sh
+++ scripts/run_python_tests.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 PIVOTAL_SOURCE=$1
-apt-get update
 
-## Install postgres
-#sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-#wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
-#apt-get -y install postgresql-contrib postgres-9.2
+gosu postgres initdb
+gosu postgres pg_ctl -D /var/lib/postgresql/data start
 
 # Configure our instance of postgres
-export PSQL_VERSION=`postgres -V | egrep -o '[0-9]{1,}\.[0-9]{1,}'`
-pg_createcluster $PSQL_VERSION main --start
-sed -i 's/peer/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
-sed -i 's/md5/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
-/etc/init.d/postgresql restart
+#export PSQL_VERSION=`postgres -V | egrep -o '[0-9]{1,}\.[0-9]{1,}'`
+#export PSQL_VERSION=9.2
+#pg_createcluster $PSQL_VERSION main --start
+#sed -i 's/peer/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
+#sed -i 's/md5/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
+#/etc/init.d/postgresql restart
+
 
 # Pass in the config file
 cp pipeline-ci/config_local.py $PIVOTAL_SOURCE/web/config_local.py
@@ -21,48 +20,23 @@ cp pipeline-ci/test_config.json $PIVOTAL_SOURCE/web/regression/test_config.json
 
 # Replace the first line of the file with the missing import
 sed -i '/__future__/a from selenium.webdriver.common.desired_capabilities import DesiredCapabilities' $PIVOTAL_SOURCE/web/regression/runtests.py
-sed -i "s/Chrome()/Remote\(command_executor='http:\/\/127.0.0.1:4444\/wd\/hub', desired_capabilities=DesiredCapabilities.CHROME\)/" ./$PIVOTAL_SOURCE/web/regression/runtests.py
+sed -i "s/Chrome()/Remote\(command_executor='http:\/\/127.0.0.1:4444\/wd\/hub', desired_capabilities=DesiredCapabilities.CHROME\)/" $PIVOTAL_SOURCE/web/regression/runtests.py
 
-/opt/bin/start_selenium.sh &
+mkdir logs
+/opt/bin/start_selenium.sh & 1>logs/selenium.out 2>logs/selenium.err
 
 source ~/.bash_profile
 
 pyenv activate pgadmin
 
-# Install project requirements
+## Install project requirements
 pip install -r $PIVOTAL_SOURCE/requirements.txt
 pip install -r $PIVOTAL_SOURCE/web/regression/requirements.txt
 
 function runTests {
-    # Run all python tests. Includes feature tests.
-    python ./$PIVOTAL_SOURCE/web/regression/runtests.py
+    python $PIVOTAL_SOURCE/web/regression/runtests.py
     status=$?
-
-    branch_name=`cd $PIVOTAL_SOURCE && git branch | grep \* | cut -d ' ' -f2`
-
-    sed -i "s/changemetobranchname/plumadmin-master/" ./pipeline-ci/manifest.yml
-
-    mkdir -p output
-    cp -r ./$PIVOTAL_SOURCE/web output
-    cp ./pipeline-ci/config_local_cf.py output/web/config_local.py
-    cp ./pipeline-ci/.cfignore output/web/
-    cp ./pipeline-ci/manifest.yml output/
-    cp ./$PIVOTAL_SOURCE/requirements.txt output/web/
-
-    mkdir -p output/web/.pgadmin/
-    cp ./pipeline-ci/database/pgadmin4-desktop.db output/web/.pgadmin/
-
     return $status
 }
 
-#service postgresql start
-
 runTests
-
-#apt-get update
-#apt-get -y install postgresql-9.5
-
-# Configure our instance of postgres
-#rm -rf /etc/postgresql/9.2
-#sed -i 's/md5/trust/' /etc/postgresql/9.5/main/pg_hba.conf
-#/etc/init.d/postgresql restart
---
+++ scripts/webpack_javascript.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+cd $INPUT_DIR/web
+
+yarn install
+yarn webpacker
+
+cd -
+
+cp -r $INPUT_DIR/* $OUTPUT_DIR
