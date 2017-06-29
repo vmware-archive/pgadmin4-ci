93860a2b0190b51bdb7c63edc5383648410deba9
George Gelashvili and Matt Kleiman <pair+ggelashvili+mkleiman@pivotal.io>
2017-06-29 14:21:42 -0400

    switch back to profile sourcing

--- scripts/run_python_tests.sh
+++ scripts/run_python_tests.sh
@@ -6,15 +6,6 @@ PIVOTAL_SOURCE=$1
 
 /opt/bin/postgres_start.sh
 
-# Configure our instance of postgres
-#export PSQL_VERSION=`postgres -V | egrep -o '[0-9]{1,}\.[0-9]{1,}'`
-#export PSQL_VERSION=9.2
-#pg_createcluster $PSQL_VERSION main --start
-#sed -i 's/peer/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
-#sed -i 's/md5/trust/' /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
-#/etc/init.d/postgresql restart
-
-
 # Pass in the config file
 cp pipeline-ci/config_local.py $PIVOTAL_SOURCE/web/config_local.py
 cp pipeline-ci/test_config.json $PIVOTAL_SOURCE/web/regression/test_config.json
@@ -26,6 +17,8 @@ sed -i "s/Chrome()/Remote\(command_executor='http:\/\/127.0.0.1:4444\/wd\/hub', 
 mkdir logs
 /opt/bin/start_selenium.sh & 1>logs/selenium.out 2>logs/selenium.err
 
+# the shell we're in is probably not a login shell in concourse
+source ~/.bash_profile
 pyenv activate pgadmin
 
 ## Install project requirements
