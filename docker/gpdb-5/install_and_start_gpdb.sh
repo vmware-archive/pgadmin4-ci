6ece4651420c0c652578f0c462f71c574da6d643
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-01-18 17:16:13 -0500

    Update the gpdb docker

--- docker/gpdb-5/install_and_start_gpdb.sh
+++ docker/gpdb-5/install_and_start_gpdb.sh
@@ -1,10 +1,11 @@
+sudo /etc/init.d/ssh start
+sleep 2
 source /home/gp/.bash_profile
 ssh-keygen -f /home/gp/.ssh/id_rsa -t rsa -N ""
 echo `hostname` > /gpdata/hostlist_singlenode
 sed -i 's/hostname_of_machine/`hostname`/g' /gpdata/gpinitsystem_singlenode
-sudo /etc/init.d/ssh start
 
-sudo su - gp bash -c 'gpssh-exkeys -f /gpdata/hostlist_singlenode'
+gpssh-exkeys -f /gpdata/hostlist_singlenode
 
 pushd /gpdata
   gpinitsystem -ac gpinitsystem_singlenode
