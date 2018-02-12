46f1236e57a699faeab2ba7839a806b1cb94eef5
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-02-12 16:45:23 -0500

    Update docker containers to have all the versions of python available

--- docker/python-selenium-chrome/pyenv_installation.sh
+++ docker/python-selenium-chrome/pyenv_installation.sh
@@ -14,4 +14,19 @@ source ~/.bash_profile
 
 pyenv install 2.7.10
 pyenv virtualenv 2.7.10 pgadmin
+pyenv install 3.1.5
+pyenv virtualenv 3.1.5 pgadmin31
+pyenv install 3.2.6
+pyenv virtualenv 3.2.6 pgadmin32
+pyenv install 3.3.7
+pyenv virtualenv 3.3.7 pgadmin33
+pyenv install 3.4.8
+pyenv virtualenv 3.4.8 pgadmin34
+pyenv install 3.5.5
+pyenv virtualenv 3.5.5 pgadmin35
+pyenv install 3.6.4
+pyenv virtualenv 3.6.4 pgadmin36
+pyenv activate pgadmin36
+
+
 pyenv activate pgadmin
