875cdefebd9054e6c46d396a57497783340f9478
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-02-26 13:05:51 -0500

    Correct variable name, and ensure error is raised when something fails

--- scripts/prepare_to_push_to_cf.sh
+++ scripts/prepare_to_push_to_cf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-branch_name=$1
+PIVOTAL_SOURCE=$1
 
 # Copy files needed for pcf to run pgAdmin4
 cp pipeline-ci/config_local_cf.py $PIVOTAL_SOURCE/web/config_local.py
@@ -9,12 +9,18 @@ cp pipeline-ci/.cfignore $PIVOTAL_SOURCE/web/
 cp pipeline-ci/manifest.yml $PIVOTAL_SOURCE/
 cp $PIVOTAL_SOURCE/requirements.txt $PIVOTAL_SOURCE/web/
 
+error=false
+
 pushd $PIVOTAL_SOURCE
   # Webpack all the things
   pushd web
-    yarn install
-    yarn run bundle
+    yarn install || error=true
+    yarn run bundle || error=true
   popd
 popd
+
+if [ $error == true ] ; then
+    exit -1
+fi
 
 exit 0
