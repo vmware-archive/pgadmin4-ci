cfc68e1cb5dbcfa1e4689fdfe96c27ec71405b37
Anthony Emengo <aemengo@pivotal.io>
2018-04-19 17:00:53 -0400

    add apply_patches task

---
+++ tasks/apply-patches/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+dir=$PWD
+
+pushd pgadmin-repo > /dev/null
+oldSHA=$(git rev-parse HEAD)
+
+git \
+  apply \
+  $dir/patches/attachments/*
+
+git \
+  -c user.name=pgadmin-bot \
+  -c user.email=pgadmin-bot@pivotal.io \
+  commit \
+  -am 'Applied patch from CI'
+
+cat > metadata.txt <<EOF
+*$(< $dir/patches/subject)*
+\`\`\`
+$(git diff ..$oldSHA --stat)
+\`\`\`
+$(ls $dir/patches/attachments/* |  xargs basename )
+EOF
+
+cat metadata.txt
+popd > /dev/null
+
+tar -czf artifacts/pgadmin-repo.tgz pgadmin-repo
