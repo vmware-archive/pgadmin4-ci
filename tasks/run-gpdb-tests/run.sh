99718774d58e7170e710c2a1e4858738af161389
Anthony Emengo <aemengo@pivotal.io>
2018-04-20 11:31:45 -0400

    Add run-postgres and run-gpdb tasks
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>

---
+++ tasks/run-gpdb-tests/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+tar -xf pgadmin-repo-tarball/*.tgz
+
+metadata=$(< pgadmin-repo/metadata.txt)
+
+payload=$(cat <<EOF
+{
+"channel": "#pgadmin4",
+"username": "patches-bot",
+"icon_emoji": ":robot_face:",
+"attachments":[
+   {
+      "fallback":"$TITLE_MESSAGE (<https://gpdb-dev.bosh.pivotalci.info/teams/pgadmin/pipelines/pgadmin-patch/jobs/run-tests|build>)",
+      "pretext":"$TITLE_MESSAGE (<https://gpdb-dev.bosh.pivotalci.info/teams/pgadmin/pipelines/pgadmin-patch/jobs/run-tests|build>)",
+      "color":"$TINT_COLOR",
+      "fields":[
+         {
+            "title":"Info",
+            "value":"$metadata",
+            "short":false
+         }
+      ]
+   }
+]
+}
+EOF
+)
+
+echo curl \
+  -X POST \
+  --data-urlencode "payload=$payload" \
+  $SLACK_URL
