fccb951ac70f7e1fe233727fe85a0f7193dee254
Anthony Emengo <aemengo@pivotal.io>
2018-04-20 12:06:53 -0400

    Update run-gpdb-test tasks
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>

--- tasks/run-gpdb-tests/run.sh
+++ tasks/run-gpdb-tests/run.sh
@@ -2,34 +2,45 @@
 
 set -e
 
+PIVOTAL_SOURCE=pgadmin-repo
+
 tar -xf pgadmin-repo-tarball/*.tgz
 
-metadata=$(< pgadmin-repo/metadata.txt)
+# Pass in the config file
+cp pgadmin-ci/config_local.py $PIVOTAL_SOURCE/web/config_local.py
+sed -e "s/{{greenplum_db_host}}/$GPDB_HOST/" \
+    -e "s/{{greenplum_db_username}}/$GPDB_USERNAME/" \
+    -e "s/{{greenplum_db_password}}/$GPDB_PASSWORD/" \
+    -e "s/{{greenplum_db_port}}/$GPDB_PORT/" pgadmin-ci/test_config.greenplum.json \
+  > $PIVOTAL_SOURCE/web/regression/test_config.json
 
-payload=$(cat <<EOF
-{
-"channel": "#pgadmin4",
-"username": "patches-bot",
-"icon_emoji": ":robot_face:",
-"attachments":[
-   {
-      "fallback":"$TITLE_MESSAGE (<https://gpdb-dev.bosh.pivotalci.info/teams/pgadmin/pipelines/pgadmin-patch/jobs/run-tests|build>)",
-      "pretext":"$TITLE_MESSAGE (<https://gpdb-dev.bosh.pivotalci.info/teams/pgadmin/pipelines/pgadmin-patch/jobs/run-tests|build>)",
-      "color":"$TINT_COLOR",
-      "fields":[
-         {
-            "title":"Info",
-            "value":"$metadata",
-            "short":false
-         }
-      ]
-   }
-]
+# Replace the first line of the file with the missing import
+sed -i '/__future__/a from selenium.webdriver.common.desired_capabilities import DesiredCapabilities' $PIVOTAL_SOURCE/web/regression/runtests.py
+sed -i "s/Chrome()/Remote\(command_executor='http:\/\/127.0.0.1:4444\/wd\/hub', desired_capabilities=DesiredCapabilities.CHROME\)/" $PIVOTAL_SOURCE/web/regression/runtests.py
+
+mkdir logs
+/opt/bin/start_selenium.sh &
+
+sudo su - gp /bin/bash -c /home/gp/install_and_start_gpdb.sh
+
+# the shell we're in is probably not a login shell in concourse
+source ~/.bash_profile
+pyenv activate pgadmin
+
+## Install project requirements
+pip install -r $PIVOTAL_SOURCE/requirements.txt
+pip install -r $PIVOTAL_SOURCE/web/regression/requirements.txt
+
+pushd $PIVOTAL_SOURCE/web
+yarn install --no-progress
+popd
+
+function runTests {
+    set +e
+    python $PIVOTAL_SOURCE/web/regression/runtests.py
+    status=$?
+    set -e
+    return $status
 }
-EOF
-)
 
-echo curl \
-  -X POST \
-  --data-urlencode "payload=$payload" \
-  $SLACK_URL
+runTests
