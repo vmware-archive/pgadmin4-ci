ee18509d418f0d184ad9cee2180ceb3014414319
Anthony Emengo <anthonyemengojr@gmail.com>
2018-04-21 08:17:29 -0400

    Add tasteful formatting around error message for ./bin/set-pipeline

--- bin/set-pipeline.sh
+++ bin/set-pipeline.sh
@@ -2,6 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 PIPELINE=${1:-pgadmin-patch}
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+lpass status > /dev/null
+if [[ $? != 0 ]]; then
+  echo -e "${RED}Error${NC}: You must log into lastpass before running this script."
+  exit 1
+fi
 
 fly \
   -t ci \
